250692849ca0dbf2c7bde3dc9e25e4cbc507d61e
pedroxavier1451 <pedro.xavier.orellana@gmail.com>
2024-07-22 11:40:42 -0500

    Update

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,27 +1,3 @@
-// import { Component, EventEmitter, Output } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { AuthService } from 'src/app/service/auth.service';
-
-// @Component({
-//   selector: 'app-login',
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.scss']
-// })
-// export class LoginComponent {
-
-//   @Output() loginSuccess = new EventEmitter<void>();
-
-//   constructor(private authService: AuthService, private router: Router) { }
-
-//   // Método para manejar el envío del formulario de inicio de sesión
-//   onLogin(): void {
-//     this.authService.login();
-//     this.loginSuccess.emit();
-//     // this.router.navigate(['/inicio']);  // Redirige a la página deseada después de iniciar sesión
-//     // location.reload(); // Recarga la página para actualizar el estado de autenticación
-//   }
-// }
-
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,67 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable, of } from 'rxjs';
-// import { map, catchError } from 'rxjs/operators';
-// import { Usuario } from 'src/domain/usuario';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthService {
-//   private apiUrl = 'http://localhost:8080/PPK/webservice/Usuario';
-
-//   constructor(private http: HttpClient) { }
-
-//   isAuthenticated(): boolean {
-//     return !!localStorage.getItem('authToken');
-//   }
-
-//   authenticate(username: string, password: string): Observable<boolean> {
-//     console.log('Autenticando usuario:', username);
-//     return this.http.get<Usuario[]>(`${this.apiUrl}/List`).pipe(
-//       map(usuarios => {
-//         const usuario = usuarios.find(user => user.usuario.trim() === username.trim() && user.password.trim() === password.trim());
-//         if (usuario) {
-//           if (usuario.estado === 'Inactivo') {
-//             alert('El usuario está inactivo.');
-//             return false;
-//           }
-//           const token = 'genericAuthToken';
-//           localStorage.setItem('authToken', token);
-//           console.log('Autenticación exitosa');
-//           return true;
-//         } else {
-//           console.log('Usuario no encontrado o credenciales incorrectas');
-//           alert('Usuario no encontrado o credenciales incorrectas');
-//           return false;
-//         }
-//       }),
-//       catchError(error => {
-//         console.error('Error durante la autenticación:', error);
-//         return of(false);
-//       })
-//     );
-//   }
-
-//   login(username: string, password: string): Observable<boolean> {
-//     return this.authenticate(username, password).pipe(
-//       map(isAuthenticated => {
-//         if (isAuthenticated) {
-//           localStorage.setItem('currentUser', username);
-//         }
-//         return isAuthenticated;
-//       })
-//     );
-//   }
-
-//   logout(): void {
-//     localStorage.removeItem('authToken');
-//     localStorage.removeItem('currentUser');
-//   }
-// }
-
-
-
 import { Injectable, numberAttribute } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
@@ -89,36 +25,6 @@ export class AuthService {
   getCurrentUserId(): string | null {
     return localStorage.getItem('userId');
   }
-
-  // authenticate(username: string, password: string): Observable<boolean> {
-  //   console.log('Autenticando usuario:', username);
-  //   return this.http.get<Usuario[]>(`${this.apiUrl}/List`).pipe(
-  //     map(usuarios => {
-  //       const usuario = usuarios.find(user => user.usuario.trim() === username.trim() && user.password.trim() === password.trim());
-  //       if (usuario) {
-  //         const token = 'genericAuthToken';
-  //         localStorage.setItem('authToken', token);
-  //         if (usuario.rol) {
-  //           localStorage.setItem('userRole', usuario.rol.descripcion);
-  //           localStorage.setItem('user', usuario.usuario);
-  //           localStorage.setItem('userId',usuario.id.toString());
-  //         } else {
-  //           localStorage.removeItem('userRole');
-  //         }
-  //         console.log('Autenticación exitosa');
-  //         return true;
-  //       } else {
-  //         console.log('Usuario no encontrado o credenciales incorrectas');
-  //         alert('Usuario no encontrado o credenciales incorrectas');
-  //         return false;
-  //       }
-  //     }),
-  //     catchError(error => {
-  //       console.error('Error durante la autenticación:', error);
-  //       return of(false);
-  //     })
-  //   );
-  // }
 
   authenticate(username: string, password: string): Observable<boolean> {
     console.log('Autenticando usuario:', username);
